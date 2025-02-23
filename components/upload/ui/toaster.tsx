936ab312891bb09e6a0c5b208fae35d58ea04743
John Doe <nithin.iitkgp@gmail.com>
2025-02-23 12:55:45 -0800

    references fix

--- components/upload/ui/toaster.tsx
+++ components/upload/ui/toaster.tsx
@@ -8,7 +8,7 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from '@/components/ui/toast';
+} from '@/components/upload/ui/toast';
 
 export function Toaster() {
   const { toasts } = useToast();
